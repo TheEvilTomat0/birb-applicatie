0e0352abab8c48622fffebd045f4269e0f1dfa4f
Leon Schoonhoven <theo-94@hotmail.com>
2023-03-06 21:09:09 +0100

    Add project files.

---
+++ Birb/Class/Bird.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birb.Class
+{
+    public class Bird
+    {
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+        private string link;
+
+        public string Link
+        {
+            get { return link; }
+            set { link = value; }
+        }
+
+    }
+}
---
+++ Birb/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Birb.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Birb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void getData(string letter)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5000/bird?bird=" + letter);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var bird = response.Content.ReadAsAsync<IEnumerable<Bird>>().Result;
+                dg.ItemsSource = bird;
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt.Text != "")
+            {
+                getData(txt.Text);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
