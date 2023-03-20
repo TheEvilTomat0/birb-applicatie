3e413b9206a1513b35257e6ac933c45577d47116
Leon Schoonhoven <theo-94@hotmail.com>
2023-03-20 20:43:41 +0100

    Basic layout + navigation & Folder structure

--- Birb/MainWindow.xaml.cs
+++ Birb/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Birb.Class;
+using Birb.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Birb
 {
@@ -24,39 +27,56 @@ namespace Birb
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void getData(string letter)
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:5000/bird?bird=" + letter);
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage response = client.GetAsync(client.BaseAddress).Result;
+            //HttpClient client = new HttpClient();
+            //client.BaseAddress = new Uri("http://localhost:5000/bird?bird=" + letter);
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //HttpResponseMessage response = client.GetAsync(client.BaseAddress).Result;
 
-            if (response.IsSuccessStatusCode)
-            {
-                var bird = response.Content.ReadAsAsync<IEnumerable<Bird>>().Result;
-                dg.ItemsSource = bird;
-            }
-            else
-            {
-                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
-            }
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    var bird = response.Content.ReadAsAsync<IEnumerable<Bird>>().Result;
+            //    dg.ItemsSource = bird;
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            //}
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            if (txt.Text != "")
+            RadioButton radioButton = (RadioButton)sender;
+            StackPanel stackPanel = (StackPanel)radioButton.Content;
+            TextBlock text = (TextBlock)stackPanel.Children[1];
+            Console.WriteLine(text.Text);
+
+            if (text.Text == "Dashboard")
             {
-                getData(txt.Text);
+                Frame.Content = new Dashboard();
             }
-            else
+            else if(text.Text == "Bird info")
+            {
+                Frame.Content = new BirdInfo();
+            } 
+            else if(text.Text == "Account")
             {
                 return;
             }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
