ce6159a9793bc6a468f47cc2286abb7c10900a15
Leon Schoonhoven <theo-94@hotmail.com>
2023-03-23 21:55:45 +0100

    Api functions

--- Birb/Classes/Bird.cs
+++ Birb/Classes/Bird.cs
@@ -1,20 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace Birb.Class
 {
     public class Bird
     {
-		private string name;
+        #region propfull
+        private string name;
 
-		public string Name
-		{
-			get { return name; }
-			set { name = value; }
-		}
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
 
         private string link;
 
@@ -23,6 +30,37 @@ namespace Birb.Class
             get { return link; }
             set { link = value; }
         }
+
+        private string height;
+
+        public string Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        private string wingSpan;
+
+        public string WingSpan
+        {
+            get { return wingSpan; }
+            set { wingSpan = value; }
+        }
+
+        private string img;
+
+        public string Img
+        {
+            get { return img; }
+            set { img = value; }
+        }
+
+        #endregion
+
+        //public string name { get; set; }
+        //public string img { get; set; }
+        //public string height { get; set; }
+        //public string wingspan { get; set; }
 
     }
 }
--- Birb/Views/BirdInfo.xaml.cs
+++ Birb/Views/BirdInfo.xaml.cs
@@ -1,7 +1,18 @@
-﻿using System;
+﻿using Birb.Class;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,11 +29,108 @@ namespace Birb.Views
     /// <summary>
     /// Interaction logic for BirdInfo.xaml
     /// </summary>
-    public partial class BirdInfo : Page
+    public partial class BirdInfo : Page, INotifyPropertyChanged
     {
+        private string birdName;
+
+        public string BirdName
+        {
+            get { return birdName; }
+            set { birdName = value; OnPropertyChanged("BirdName"); getBirdList(); }
+        }
+
+        public string SelectedBird { get; set; }
+
+        private ObservableCollection<Bird> birds;
+
+        public ObservableCollection<Bird> Birds
+        {
+            get { return birds; }
+            set { birds = value; OnPropertyChanged("Birds"); }
+        }
+
+        private Bird bird;
+
+        public Bird Bird
+        {
+            get { return bird; }
+            set { bird = value; OnPropertyChanged("Bird"); }
+        }
+
+
         public BirdInfo()
         {
             InitializeComponent();
+            DataContext = this;
         }
+
+        private void getBirdList()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://bird-api.codeonmedia.nl/birds/?search=" + BirdName);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                Birds = response.Content.ReadAsAsync<ObservableCollection<Bird>>().Result;
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }        
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox listBox = (ListBox)sender;
+            SelectedBird = listBox.SelectedValue.ToString();
+
+            if (SelectedBird != null)
+            {
+                getBirdData();
+            }
+        }
+
+        private void getBirdData()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://bird-api.codeonmedia.nl/bird/?bird=" + SelectedBird);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                //object bird = response.Content.ReadAsAsync<ObservableCollection<Bird>>().Result;
+                var json = response.Content.ReadAsStringAsync().Result;
+                var obj = JsonConvert.DeserializeObject<IList<Bird>>(json);
+                Bird = obj[0];
+                Bird.Img.Replace("&", "&amp;");
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+
+            #region TestCode
+            //using (var client = new HttpClient())
+            //{
+            //    var endpoint = "https://bird-api.codeonmedia.nl/bird/?bird=" + SelectedBird;
+            //    var result = client.GetAsync(endpoint).Result;
+            //    //var json = JsonConvert.SerializeObject(result);
+            //    var json = result.Content.ReadAsStringAsync().Result;
+            //    var obj = JsonConvert.DeserializeObject<IList<Bird>>(json);
+            //    Bird = obj[0];
+            //}
+            #endregion
+        }
+
+
     }
 }
